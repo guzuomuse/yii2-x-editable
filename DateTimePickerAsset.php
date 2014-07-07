c291362cf9915f4992166399517fd885bf47aed2
guzuomuse <253840975@qq.com>
2014-07-07 14:29:52 +0800

    Update DateTimePickerAsset.php

--- DateTimePickerAsset.php
+++ DateTimePickerAsset.php
@@ -15,22 +15,22 @@ class DateTimePickerAsset extends AssetBundle
 {
 
 	public $sourcePath = '@xeditable/assets/';
-
-	public $js = [
-		"bootstrap-datetimepicker/js/bootstrap-datetimepicker.js"
-	];
-
-	public $css = [
-		"bootstrap-datetimepicker/css/bootstrap-datetimepicker.css",
-	];
-
-	public $publishOptions = [
-		'forceCopy' => true
-	];
-
-	public $depends = [
-		'yii\web\YiiAsset',
-		'yii\bootstrap\BootstrapPluginAsset',
-	];
+//
+//	public $js = [
+////		"bootstrap-datetimepicker/js/bootstrap-datetimepicker.js" //because the bootstrap-datetimepicker.js file has benn included in bootstrap-editable.(min.) js，so this is no requirment
+//	];
+//
+//	public $css = [
+////		"bootstrap-datetimepicker/css/bootstrap-datetimepicker.css",
+//	];
+//
+//	public $publishOptions = [
+//		'forceCopy' => true
+//	];
+//
+//	public $depends = [
+//		'yii\web\YiiAsset',
+//		'yii\bootstrap\BootstrapPluginAsset',
+//	];
 
 }
