12b61b4b41d52ab208a59edd1372065040f9a679
guzuomuse <253840975@qq.com>
2014-07-07 14:15:29 +0800

    Update DatePickerAsset.php

--- DatePickerAsset.php
+++ DatePickerAsset.php
@@ -17,12 +17,14 @@ class DatePickerAsset extends AssetBundle
 	public $sourcePath = '@xeditable/assets/';
 
 	public $js = [
-		"bootstrap-datepicker/js/bootstrap-datepicker.js"
+//                "bootstrap-datepicker/js/bootstrap-datepicker.js",//because the bootstrap-datepicker.js file has benn included in bootstrap-editable.(min.) js，so this is no requirment 
+                
+		               
 	];
 
 	public $css = [
-		"bootstrap-datepicker/css/datepicker3.css",
-		"bootstrap-datepicker/css/datepicker-kv.css",
+//		"bootstrap-datepicker/css/datepicker3.css",
+//		"bootstrap-datepicker/css/datepicker-kv.css",
 	];
 
 	public $publishOptions = [
@@ -32,6 +34,7 @@ class DatePickerAsset extends AssetBundle
 	public $depends = [
 		'yii\web\YiiAsset',
 		'yii\bootstrap\BootstrapPluginAsset',
+            
 	];
 
 }
